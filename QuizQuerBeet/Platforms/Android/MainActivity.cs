2e1c056999ca41f010600560423f0b24b2731935
Fabian Dasler <fabian@MacBook-Pro-von-Fabian.fritz.box>
2022-10-22 13:24:40 +0200

    add app icon

--- QuizQuerBeet/Platforms/Android/MainActivity.cs
+++ QuizQuerBeet/Platforms/Android/MainActivity.cs
@@ -4,7 +4,13 @@ using Android.OS;
 
 namespace QuizQuerBeet;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Label = "Quiz Quer Beet", Theme = "@style/Maui.SplashTheme",
+           MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize
+                                | ConfigChanges.Orientation
+                                | ConfigChanges.UiMode
+                                | ConfigChanges.ScreenLayout
+                                | ConfigChanges.SmallestScreenSize
+                                | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
