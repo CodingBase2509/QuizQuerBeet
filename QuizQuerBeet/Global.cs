42b1c7d59d337be4f3eb58777c0e3e06df609cb8
Fabian <famabiker@gmail.com>
2022-11-04 13:58:32 +0100

    minor changes

--- QuizQuerBeet/Global.cs
+++ QuizQuerBeet/Global.cs
@@ -2,6 +2,8 @@
 global using QuizQuerBeet.Views;
 global using QuizQuerBeet.ViewModels;
 
+global using QuizQuerBeet.Infrastructure.Contracts;
+
 global using System.Linq.Expressions;
 
 global using CommunityToolkit.Mvvm.ComponentModel;
