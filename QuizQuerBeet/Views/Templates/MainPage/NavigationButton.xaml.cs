fa8d55397efd95e0cec3c2fbfb4060778084fa51
Fabian <famabiker@gmail.com>
2022-11-04 08:10:18 +0100

    refactoring of the Navigation button to remove the tapgesture

--- QuizQuerBeet/Views/Templates/MainPage/NavigationButton.xaml.cs
+++ QuizQuerBeet/Views/Templates/MainPage/NavigationButton.xaml.cs
@@ -5,8 +5,14 @@ namespace QuizQuerBeet.Views.Templates.MainPage;
 
 public partial class NavigationButton : ContentView
 {
-    static readonly BindableProperty TextProperty = BindableProperty
+    public static readonly BindableProperty TextProperty = BindableProperty
         .Create(nameof(Text), typeof(string), typeof(NavigationButton), string.Empty);
+
+    public static readonly BindableProperty CommandProperty = BindableProperty
+        .Create(nameof(Command), typeof(ICommand), typeof(NavigationButton), null);
+
+    public static readonly BindableProperty CommandParameterProperty = BindableProperty
+        .Create(nameof(CommandParameter), typeof(object), typeof(NavigationButton));
 
     public NavigationButton()
     {
@@ -17,5 +23,17 @@ public partial class NavigationButton : ContentView
     {
         get => (string)GetValue(TextProperty);
         set => SetValue(TextProperty, value);
+    }
+
+    public ICommand Command
+    {
+        get => GetValue(CommandProperty) as ICommand;
+        set => SetValue(CommandProperty, value);
+    }
+
+    public object CommandParameter
+    {
+        get => GetValue(CommandParameterProperty) as object;
+        set => SetValue(CommandParameterProperty, value);
     }
 }
