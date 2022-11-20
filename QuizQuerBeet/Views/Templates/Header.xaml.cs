8864506c84737340976d75a66ac7f875b9506e78
Fabian <famabiker@gmail.com>
2022-11-20 14:28:37 +0100

    add a page header

---
+++ QuizQuerBeet/Views/Templates/Header.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace QuizQuerBeet.Views.Templates;
+
+public partial class Header : ContentView
+{
+	public static readonly BindableProperty TextProperty = BindableProperty
+		.Create(nameof(Text), typeof(string), typeof(Header), string.Empty);
+
+	public Header()
+	{
+		InitializeComponent();
+	}
+
+	public string Text
+    {
+		get => GetValue(TextProperty) as string;
+		set => SetValue(TextProperty, value);
+    }
+}
