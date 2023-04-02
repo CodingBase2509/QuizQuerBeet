340311fe227e750e6e2e98e2f2b0e876c0f9c3ab
fabian <famabiker@gmail.com>
2023-04-02 12:34:41 +0200

    remove registering of pages that are registered in the Tabbar, because of causing routing errors

--- QuizQuerBeet/Extentions/RoutingExtentions.cs
+++ QuizQuerBeet/Extentions/RoutingExtentions.cs
@@ -6,10 +6,6 @@ public static class ShellExtentions
 {
     public static void RegisterRoutes()
     {
-        Routing.RegisterRoute("mainPage", typeof(MainPage));
-        Routing.RegisterRoute("statistics", typeof(Statistics));
-        Routing.RegisterRoute("categories", typeof(CategorieView));
-
         Routing.RegisterRoute("quizEditing", typeof(QuizEditing));
         Routing.RegisterRoute("questionEditing", typeof(QuestionEditing));
     }
